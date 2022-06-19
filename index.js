26912a4a3ae132a79839bddfce14e0b05c6a79e5
Alexander Cortez <acortez8988@gmail.com>
2022-06-19 18:02:42 -0400

    commit tree 95b1b9b948d81dee915a5e2569131e1e2edaf14d

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const name = ["John", "Ari", "Sam"];
+const event = 'birthday'
+
+
+function writeCards(name, event) {
+    const newArry =[]
+  for (let i = 0; i < name.length; i++) {
+    newArry.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return newArry;
+}
+writeCards(name, event)
+
+function countDown() {
+  let num = 10;
+  while (num >=0) { 
+    console.log(num--);
+    
+  }
+}
